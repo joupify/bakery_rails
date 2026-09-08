db3badac1b30ea79976578b14e986faf59cefc4e
Malika Housni <mikayakouta@gmail.com>
2026-09-08 23:41:00 +0200

    compile assets true in prod

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,6 +1,8 @@
 # config/initializers/assets.rb
 Rails.application.config.assets.version = "1.0"
 
+Rails.application.config.assets.precompile += %w[custom.css]
+
 # Ajouter les chemins
 Rails.application.config.assets.paths << Rails.root.join("node_modules/bootstrap-icons/font")
 Rails.application.config.assets.paths << Rails.root.join("node_modules/bootstrap/dist/js")
